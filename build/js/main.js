27afa8d160db310b2358b5a1ecdc3c9c9d3c2921
Maria Aleksandrova <mail@aleksandrovamaria.ru>
2021-04-21 13:23:02 +0300

    fix: IIFE, decor-circle transform

--- build/js/main.js
+++ build/js/main.js
@@ -1,39 +1,44 @@
 'use strict';
 
-const headerMenu = document.querySelector(`.header__menu`);
-const headerToggle = document.querySelector(`.header__toggle`);
+(function () {
+  const headerMenu = document.querySelector(`.header__menu`);
+  const headerToggle = document.querySelector(`.header__toggle`);
 
 
-/**
- * Инициализирует внешний вид меню при работающем сценарии
- * @returns void
- */
-function setInitialMenu() {
-  headerToggle.classList.remove(`header__toggle--nojs`);
-  headerMenu.classList.remove(`menu--nojs`);
-}
-
-
-/**
- * Ловит событие click на меню-гамбургере
- * @param {event} evt
- * @returns void
- */
-function toggleClickHandler(evt) {
-  evt.preventDefault();
-  headerMenu.classList.toggle(`menu--open`);
-  headerToggle.classList.toggle(`header__toggle--open`);
-
-  let menuLinks = headerMenu.querySelectorAll(`.menu__link`);
-  for (let link of menuLinks) {
-    link.addEventListener(`click`, function() {
-      headerMenu.classList.remove(`menu--open`);
-      headerToggle.classList.remove(`header__toggle--open`);
-    });
+  /**
+   * Инициализирует внешний вид меню при работающем сценарии
+   * @returns void
+   */
+  function setInitialMenu() {
+    headerToggle.classList.remove(`header__toggle--nojs`);
+    headerMenu.classList.remove(`menu--nojs`);
   }
-}
 
-if (headerMenu && headerToggle) {
-  setInitialMenu();
-  headerToggle.addEventListener(`click`, toggleClickHandler);
-}
+
+  /**
+   * Ловит событие click на меню-гамбургере
+   * @param {event} evt
+   * @returns void
+   */
+  function toggleClickHandler(evt) {
+    evt.preventDefault();
+    headerMenu.classList.toggle(`menu--open`);
+    headerToggle.classList.toggle(`header__toggle--open`);
+
+    let menuLinks = headerMenu.querySelectorAll(`.menu__link`);
+
+    if (menuLinks) {
+      for (let link of menuLinks) {
+        link.addEventListener(`click`, function() {
+          headerMenu.classList.remove(`menu--open`);
+          headerToggle.classList.remove(`header__toggle--open`);
+        });
+      }
+    }
+  }
+
+  if (headerMenu && headerToggle) {
+    setInitialMenu();
+    headerToggle.addEventListener(`click`, toggleClickHandler);
+  }
+})();
